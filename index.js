6bee7c4bcab7e422f119a1eef1451c6d01ce67e2
zhouck <changkun.zhou@meicloud.com>
2019-05-20 23:13:40 +0800

    first

---
+++ index.js
@@ -0,0 +1,65 @@
+/**
+ * =================================================
+ * toast
+ * Created by zhouck on 2016/04/07
+ * =================================================
+ */
+;
+(function(undefined) {
+	"use strict"
+
+	var _global;
+
+	// 插件构造函数 - 返回数组结构
+	function MyToast(opt) {
+		this._initial(opt);
+	}
+	MyToast.prototype = {
+		constructor: this,
+		_initial: function(opt) {
+			// 默认参数
+			var def = {
+				message: '',
+				time: 1500
+			};
+			//配置参数
+			this.def = Object.assign(def, opt);
+		},
+		show: function() {
+			// toast停留时间
+			var time = this.def.time;
+			var el = document.createElement("div");
+			el.setAttribute("class", "z-toast");
+			el.innerHTML = this.def.message;
+			document.body.appendChild(el);
+			el.classList.add("fadeIn");
+			setTimeout(function() {
+				el.classList.remove("fadeIn");
+				el.classList.add("fadeOut");
+				// 监听动画结束，移除toast
+				el.addEventListener("animationend", function() {
+					document.body.removeChild(el);
+				});
+				el.addEventListener("webkitAnimationEnd", function() {
+					document.body.removeChild(el);
+				});
+
+			}, time);
+		},
+
+	}
+
+	// 最后将插件对象暴露给全局对象
+	_global = (function() {
+		return this || (0, eval)('this');
+	}());
+	if(typeof module !== "undefined" && module.exports) {
+		module.exports = MyToast;
+	} else if(typeof define === "function" && define.amd) {
+		define(function() {
+			return MyToast;
+		});
+	} else {
+		!('MyToast' in _global) && (_global.MyToast = MyToast);
+	}
+}());
